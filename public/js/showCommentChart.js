4180a48b91adb170d4a3527b499d7428832e51e6
mataotao <mttwind@foxmail.com>
2019-05-02 12:12:51 +0800

    添加阅读数量和评论数的数据饼图

---
+++ public/js/showCommentChart.js
@@ -0,0 +1,73 @@
+var allCommentCharts = document.querySelectorAll('.commentChart')
+allCommentCharts.forEach(function (item) {
+
+    var contentTitle = item.getAttribute('contentTitle')
+    var contentViews = Number(item.getAttribute('contentViews'))
+    var commentsLength = Number(item.getAttribute('commentsLength'))
+    
+    var myChart = echarts.init(item);
+    var option = null;
+    option = {
+        title : {
+            text: contentTitle+'数据饼图',
+            subtext: '学习次数与评论次数对比图',
+            x:'center'
+        },
+        tooltip : {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            data: ['课程学习数','评论数']
+        },
+        series : [
+            {
+                name: '访问来源',
+                type: 'pie',
+                radius : '55%',
+                center: ['50%', '60%'],
+                data:[
+                    {value:contentViews, name:'课程学习数'},
+                    {value:commentsLength, name:'评论数'},
+                ],
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+    ;
+    if (option && typeof option === "object") {
+        myChart.setOption(option, true);
+    }
+})
+var myChart = echarts.init(document.getElementById('main'));
+
+// 指定图表的配置项和数据
+var option = {
+    title: {
+        text: 'ECharts 入门示例'
+    },
+    tooltip: {},
+    legend: {
+        data: ['销量']
+    },
+    xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+    },
+    yAxis: {},
+    series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+    }]
+};
+
+// 使用刚指定的配置项和数据显示图表。
+myChart.setOption(option);
